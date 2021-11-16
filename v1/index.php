747651f36e9b3666911ce061d124c1c2354ebf59
JeanPiffaut <piffaut.jean@gmail.com>
2021-11-16 02:55:37 -0300

    add htaccess

--- v1/index.php
+++ v1/index.php
@@ -4,9 +4,11 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 header("Access-Control-Max-Age: 3600");
 
-include_once dirname(__DIR__) . "/v2/product.php";
+$service = $_REQUEST['service'];
 
-$product = new product();
+include_once dirname(__DIR__) . "/v2/" . $service . ".php";
+
+$product = new $service();
 
 switch ($_SERVER['REQUEST_METHOD'])
 {
