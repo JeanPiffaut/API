974584fec46d99beb5d283d44a40cda1ececeb01
JeanPiffaut <piffaut.jean@gmail.com>
2021-12-05 19:29:04 -0300

    update

--- v3/src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$CONFIG = array();
-
-global $CONFIG;
-
-$CONFIG['project']['version'] = "v3";
-$CONFIG['project']['default_page'] = "home";
-$CONFIG['access']['methods'] = ["GET", "POST", "PUT", "PATCH", "DELETE"];
