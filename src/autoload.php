5b1fd0aa9bcb900515688473c97c6355aec12d83
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2021-12-05 19:30:46 -0300

    update (#2)

--- src/autoload.php
+++ src/autoload.php
@@ -4,4 +4,5 @@
 include_once __DIR__ . "/config.php";
 
 // add endpoint files
+include_once __DIR__ . "/endpoint_result.php";
 include_once __DIR__ . "/EndPoint.php";
--- v3/controller/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once __DIR__ . "/../src/EndPoint.php";
-
-class home extends EndPoint
-{
-    public string $poto;
-}
